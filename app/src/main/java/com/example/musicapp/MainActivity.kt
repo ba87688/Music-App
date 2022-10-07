28f861ce0e56d444bf429ccacc2c09284b9a24ea
Jagi <ba87688@gmail.com>
2022-10-07 07:33:56 -0500

    permissions to record

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.musicapp
 
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
@@ -13,18 +16,45 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    lateinit var mr:MediaRecorder
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.isEnabled = false
+        var path = Environment.getExternalStorageDirectory().toString() +"/myrec.3gp"
+        mr = MediaRecorder()
+
+        binding.button.isEnabled = true
         binding.button2.isEnabled = false
 
         if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.RECORD_AUDIO)!=PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.RECORD_AUDIO,android.Manifest.permission.WRITE_EXTERNAL_STORAGE),111)
             binding.button.isEnabled = true
         }
+        binding.button.setOnClickListener {
+            mr.setAudioSource(MediaRecorder.AudioSource.MIC)
+            mr.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            mr.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            mr.setOutputFile(path)
+            mr.prepare()
+            mr.start()
+            binding.button2.isEnabled=true
+            binding.button.isEnabled = false
+        }
+
+        binding.button2.setOnClickListener {
+            mr.stop()
+            binding.button.isEnabled = true
+            binding.button2.isEnabled = false
+        }
+        binding.button3.setOnClickListener {
+            var mp = MediaPlayer()
+            mp.setDataSource(path)
+            mp.prepare()
+            mp.start()
+        }
+
     }
 
     override fun onRequestPermissionsResult(
